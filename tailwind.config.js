3d1723a3df4c2237a1ebc2d878dc46e6a0b19fd8
Stevens <u20201e735@upc.edu.pe>
2023-09-09 02:11:00 -0500

    initial state of the landing page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#3b82f6",
+          secondary: "#2a9ede",
+          tertiary: "#30e8fa"
+        }
+      },
+    },
+    plugins: [],
+  }
